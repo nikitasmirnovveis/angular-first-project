ad42da93f60c1f2afd0ab72694fac70af40b0b3e
nikitasmirnovveis <71231869+nikitasmirnovveis@users.noreply.github.com>
2024-01-25 12:09:59 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-first-project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nikitasmirnovveis/angular-first-project)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+// import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cart',
+  // standalone: true,
+  // imports: [CommonModule],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css'
+})
+export class CartComponent {
+
+  items = this.cartService.getItems();
+
+  constructor(
+    private cartService: CartService
+  ) { }
+}
